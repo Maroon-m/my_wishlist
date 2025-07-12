73a147fe5244fdc91a4e0fbc6210ed12f11ab6e2
Mariia <119541582+Maroon-m@users.noreply.github.com>
2025-07-12 20:01:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,19 +20,39 @@ def verify_telegram(data):
 
 def get_db():
     conn = sqlite3.connect('db.sqlite', check_same_thread=False)
-    conn.execute('''CREATE TABLE IF NOT EXISTS gifts(id INTEGER PRIMARY KEY, title, description, link)''')
+    conn.execute('''CREATE TABLE IF NOT EXISTS gifts(id INTEGER PRIMARY KEY, title, description, link, category)''')
     conn.execute('''CREATE TABLE IF NOT EXISTS reserves(gift_id, tg_id, username, timestamp)''')
+
+    cur = conn.execute('SELECT COUNT(*) FROM gifts')
+    if cur.fetchone()[0] == 0:
+        sample = [
+            (1, "Модель Breyer Catch Me", "Можно выловить на Авито", "https://www.breyerhorses.com/products/catch-me", "Самые желанные"),
+            (2, "Instax фотоаппарат мгновенной печати", "Или беленький полароид, но он, кажется, ещё дороже :(", "https://www.ozon.ru/product/fotoapparat-mgnovennoy-pechati-fujifilm-mini-12-zelenyy-1047331780/?at=DqtD7yOAAS5PvzxzHJl6BlPuJNJGRvHGMDmR3F3MgK4M", "Самые желанные"),
+            (3, "Сессия с психологом", "", "", "Полезности"),
+            (4, "МРФ-ролик", "", "", "Полезности"),
+            (5, "Ручной отпариватель", "", "", "Полезности"),
+            (6, "Набор кухонных полотенец из икеи", "", "", "Полезности"),
+            (7, "Диффузор Hygge #4", "Источник гармонии", "", "Приятности"),
+            (8, "Скраб для тела", "Обожаю лемонграсс или что-то подобное", "", "Приятности"),
+            (9, "Энзимная пудра для умывания", "", "", "Приятности"),
+            (10, "Протеиновые вкусняхи", "(без шоколада)", "", "Приятности"),
+            (11, "Сертификаты Ozon, Золотое Яблоко", "", "", "Универсальное")
+        ]
+        conn.executemany('INSERT INTO gifts VALUES (?,?,?,?,?)', sample)
+        conn.commit()
+
     return conn
+
 
 db = get_db()
 
 @app.route('/wishlist')
 def wishlist():
-    cur = db.execute('SELECT * FROM gifts')
+    cur = db.execute('SELECT id, title, description, link, category FROM gifts')
     gifts = []
     for row in cur.fetchall():
-        status = db.execute('SELECT count(*) FROM reserves WHERE gift_id=?', (row[0],)).fetchone()[0] > 0
-        gifts.append(dict(id=row[0], title=row[1], desc=row[2], link=row[3], reserved=status))
+        reserved = db.execute('SELECT count(*) FROM reserves WHERE gift_id=?', (row[0],)).fetchone()[0] > 0
+        gifts.append(dict(id=row[0], title=row[1], desc=row[2], link=row[3], category=row[4], reserved=reserved))
     return jsonify(gifts)
 
 @app.route('/reserve', methods=['POST'])
@@ -77,19 +97,6 @@ def reset():
     db.execute('DELETE FROM reserves WHERE gift_id=?', (gift_id,))
     db.commit()
     return "OK"
-
-@app.route('/init')
-def init_db():
-    sample = [
-        (1, "Модель Breyer Catch Me", "Можно выловить на Авито", "https://www.breyerhorses.com/products/catch-me"),
-        (2, "Instax фотоаппарат", "Или белый Polaroid, но он дороже", "https://www.ozon.ru/product/fotoapparat-mgnovennoy-pechati-fujifilm-mini-12-zelenyy-1047331780/?at=..."),
-        (3, "Сессия с психологом", "", ""),
-        (4, "МРФ-ролик", "", ""),
-        (5, "Ручной отпариватель", "", "")
-    ]
-    db.executemany('INSERT OR IGNORE INTO gifts VALUES (?,?,?,?)', sample)
-    db.commit()
-    return "База заполнена"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
